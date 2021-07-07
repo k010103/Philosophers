95de46fe713f4b7bb6b09aa043cbffcd3528dcba
junmkang <k010103@naver.com>
2021-07-07 10:09:50 +0900

    phio_loop 수정.

--- philo/srcs/philo_loop.c
+++ philo/srcs/philo_loop.c
@@ -6,7 +6,7 @@
 /*   By: junmkang <junmkang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 15:19:18 by junmkang          #+#    #+#             */
-/*   Updated: 2021/07/05 18:06:22 by junmkang         ###   ########.fr       */
+/*   Updated: 2021/07/07 10:09:27 by junmkang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,20 @@ void	*philo_action(void *v_pthread)
 	return (NULL);
 }
 
+// pthread_create 함수가 실패하였을경우 에러처리를 꼭 해줘야할까..?
+
+static int	create_philos_must_eat(t_info *info)
+{
+	int			error;
+	pthread_t	monitor;
+
+	error = pthread_create(&monitor, NULL, philo_must_eat, (void *)info);
+	if (error)
+		return (_ERROR);
+	pthread_detach(monitor);
+	return (_OK);
+}
+
 int	create_philos(t_info *info)
 {
 	int			count;
@@ -46,17 +60,13 @@ int	create_philos(t_info *info)
 			return (_ERROR);
 		error = pthread_create(&monitor, NULL, \
 								philo_monitor, (void *)&info->philos[count]);
-		pthread_detach(monitor);
 		if (error)
 			return (_ERROR);
+		pthread_detach(monitor);
 		count++;
 	}
 	if (info->must_eat != 0 && info->philo_num > 1)
-	{
-		error = pthread_create(&monitor, NULL, philo_must_eat, (void *)info);
-		if (error)
-			return (_ERROR);
-	}
+		create_philos_must_eat(info);
 	return (_OK);
 }
 
